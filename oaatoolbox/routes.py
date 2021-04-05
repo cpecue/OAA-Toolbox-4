20de75f9e9461737b55b7fd2b11fe15bcb715f01
cpecue <caleb.pecue.1@gmail.com>
2021-04-05 15:58:56 -0500

    Copying Toolbox

--- oaatoolbox/routes.py
+++ oaatoolbox/routes.py
@@ -95,7 +95,7 @@ def selenium():
     studentFN = request.form['studentFN']
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
