0393f0df7be7408683d92e6aa4f5c48a9227c616
cpecue <caleb.pecue.1@gmail.com>
2021-04-05 18:26:06 -0500

    Changing Selenium

--- oaatoolbox/routes.py
+++ oaatoolbox/routes.py
@@ -93,14 +93,16 @@ def account():
 @app.route("/_runSelenium", methods=['GET', 'POST'])
 def selenium():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = os.getenv("GOOGLE_CHROME_BIN")
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(executable_path=os.environ.get('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
     driver.get('https://google.com/')
     print("seems to work")
     # driver.close()
+    return render_template('_runSelenium.html', title='Selenium')
 
 
 @app.route('/declare')
