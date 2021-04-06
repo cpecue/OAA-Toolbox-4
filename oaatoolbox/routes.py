ef9cdf417230f2830fe13c6efdaac9e1b3e4ceb0
cpecue <caleb.pecue.1@gmail.com>
2021-04-05 23:05:50 -0500

    Changing Selenium

--- oaatoolbox/routes.py
+++ oaatoolbox/routes.py
@@ -97,7 +97,6 @@ def selenium():
     e_password = request.form['advisorPw']
     studentFN = request.form['studentFN']
     studentMajor = request.form['studentMajor']
-    code = request.form['code']
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.getenv("GOOGLE_CHROME_BIN")
     chrome_options.add_argument('--headless')
@@ -126,7 +125,7 @@ def selenium():
     yes = driver.find_element_by_xpath(
         '/html/body/div/form/div/div/div[1]/div[2]/div/div[2]/div/div[3]/div[2]/div/div/div[2]/input')
     yes.click()
-    print(f'successfully logged in as {code}')
+    print(f'successfully logged in as {studentMajor}')
     major = driver.find_element_by_xpath(
         '/html/body/div/div/div/div/div/div/div[1]/div[2]/div[2]/div/div/div[2]/div/div/input')
     major.send_keys(code)
