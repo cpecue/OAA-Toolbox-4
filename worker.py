9ba436591853b6d185eeaa6f14e4c83024e6c483
cpecue <caleb.pecue.1@gmail.com>
2021-04-15 18:24:48 -0500

    Reorganizing the selenium defs.

--- worker.py
+++ worker.py
@@ -5,7 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('redis://redistogo:08d2170431f53a949a39c0667126e467', 'scat.redistogo.com:10064/')
 
 conn = redis.from_url(redis_url)
 
