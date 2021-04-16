963ae2fdfde03c6b1c756c2fd9f5dbce8ec322ca
cpecue <caleb.pecue.1@gmail.com>
2021-04-16 09:50:10 -0500

    Changing back to the RQ to add background worker.

--- oaatoolbox/routes.py
+++ oaatoolbox/routes.py
@@ -37,100 +37,100 @@ class MyWorker():
     # do something
 
 
-def to_background():
+def to_background(e_ID, e_password, effective_term_text, studentFN, studentLN, studentID, studentEmail, studentPhone, requester, status_text, collegeCode, degreeCode, majorCode):
     time.sleep(3)
     print('It successfully grabs the queue')
-    # # Selenium setup
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.binary_location = os.getenv("GOOGLE_CHROME_BIN")
-    # chrome_options.add_argument('--headless')
-    # chrome_options.add_argument('--disable-dev-shm-usage')
-    # chrome_options.add_argument('--disable-gpu')
-    # chrome_options.add_argument('--no-sandbox')
-    # driver = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
-    # time.sleep(3)
-    # # This is the website form:
-    # driver.get('https://forms.office.com/Pages/ResponsePage.aspx?id=IX3zmVwL6kORA-FvAvWuzwdoWEcc_LNCksX8Xu0GatNUMFAwSDJONjZXUTBUN0NDRklTS1ZJRlpOSi4u')
-    # time.sleep(3)
-    # # Login Screen
-    # email_input = driver.find_element_by_xpath('//*[@id="i0116"]')  # email input
-    # email_input.send_keys(e_ID)
-    # email_input.send_keys(Keys.ENTER)
-    # time.sleep(3)
-    # # Password Screen
-    # password_input = driver.find_element_by_xpath('//*[@id="i0118"]')  # password input
-    # password_input.send_keys(e_password)
-    # password_input.send_keys(Keys.ENTER)
-    # time.sleep(3)
-    # # Reduce Sign-ins Page
-    # yes_btn = driver.find_element_by_xpath('//*[@id="idSIButton9"]')  # Yes button on reduced sign in page
-    # yes_btn.click()
-    # print(f'successfully logged in as {e_ID}.')
-    # time.sleep(2)
-    # #  Page 1
-    # # Primary Program
-    # primary_program = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[1]/div/div[2]/div/div[1]/div/label/input')  # Setting primary program to true for first declaration
-    # primary_program.click()
-    # print('Primary program clicked...')
-    #
-    # # Effective Term
-    # if effective_term_text == "This Semester":
-    #     primary_program.send_keys(Keys.TAB, Keys.SPACE)
-    # else:
-    #     primary_program.send_keys(Keys.TAB, Keys.TAB, Keys.SPACE)
-    # print(f'Effective term is set to {effective_term_text}.')
-    #
-    # # Student's Name
-    # student_name = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[3]/div/div[2]/div/div/input')
-    # student_name.send_keys(studentFN + ' ' + studentLN)
-    # print(f'Sending student\'s full name as {studentFN} {studentLN}. ')
-    #
-    # # Student's ID
-    # student_id = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[4]/div/div[2]/div/div/input')
-    # student_id.send_keys(studentID)
-    # print(f'Sending student\'s id as {studentID}.')
-    #
-    # # Student's Email
-    # student_email_input = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[5]/div/div[2]/div/div/input')
-    # student_email_input.send_keys(studentEmail)
-    # print(f'Sending student\'s email as {studentEmail}.')
-    #
-    # # Student's Phone
-    # student_phone_input = driver.find_element_by_xpath(
-    #     '//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[6]/div/div[2]/div/div/input')
-    # student_phone_input.send_keys(studentPhone)
-    # print(f'Sending student\'s phone as {studentPhone}.')
-    #
-    # # Requester Name
-    # requester_input = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[7]/div/div[2]/div/div/input')
-    # requester_input.send_keys(requester)
-    # print(f'Form prepared by {requester}')
-    #
-    # if status_text == "Undeclared":
-    #     print('from Undeclared')
-    #     from_college_code = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[8]/div/div[2]/div/div[2]/div/label/input')
-    #     from_college_code.click()
-    #     from_degree_code = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[9]/div/div[2]/div/div/input')
-    #     from_degree_code.send_keys('00')
-    #     teacher_cert = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[21]/div/div[2]/div/div[2]/div/label/input')
-    #     teacher_cert.click()
-    # else:
-    #     pass
-    #
-    # print(f'Declaring student with College Code of {collegeCode}')
-    # if collegeCode == "AS":
-    #     print('This is the AS option')
-    #     college_to_btn = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[22]/div/div[2]/div/div[1]/div/label/input')
-    #     college_to_btn.click()
-    # else:
-    #     pass
-    #
-    # to_degree_code = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[23]/div/div[2]/div/div/input')
-    # to_degree_code.send_keys(degreeCode)
-    # print(f'Sending degree code of {degreeCode}')
-    # print(f'Sending majorCode of {majorCode}')
-    #
-    # driver.close()
+    # Selenium setup
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.getenv("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(executable_path=os.getenv('CHROMEDRIVER_PATH'), chrome_options=chrome_options)
+    time.sleep(3)
+    # This is the website form:
+    driver.get('https://forms.office.com/Pages/ResponsePage.aspx?id=IX3zmVwL6kORA-FvAvWuzwdoWEcc_LNCksX8Xu0GatNUMFAwSDJONjZXUTBUN0NDRklTS1ZJRlpOSi4u')
+    time.sleep(3)
+    # Login Screen
+    email_input = driver.find_element_by_xpath('//*[@id="i0116"]')  # email input
+    email_input.send_keys(e_ID)
+    email_input.send_keys(Keys.ENTER)
+    time.sleep(3)
+    # Password Screen
+    password_input = driver.find_element_by_xpath('//*[@id="i0118"]')  # password input
+    password_input.send_keys(e_password)
+    password_input.send_keys(Keys.ENTER)
+    time.sleep(3)
+    # Reduce Sign-ins Page
+    yes_btn = driver.find_element_by_xpath('//*[@id="idSIButton9"]')  # Yes button on reduced sign in page
+    yes_btn.click()
+    print(f'successfully logged in as {e_ID}.')
+    time.sleep(2)
+    #  Page 1
+    # Primary Program
+    primary_program = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[1]/div/div[2]/div/div[1]/div/label/input')  # Setting primary program to true for first declaration
+    primary_program.click()
+    print('Primary program clicked...')
+
+    # Effective Term
+    if effective_term_text == "This Semester":
+        primary_program.send_keys(Keys.TAB, Keys.SPACE)
+    else:
+        primary_program.send_keys(Keys.TAB, Keys.TAB, Keys.SPACE)
+    print(f'Effective term is set to {effective_term_text}.')
+
+    # Student's Name
+    student_name = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[3]/div/div[2]/div/div/input')
+    student_name.send_keys(studentFN + ' ' + studentLN)
+    print(f'Sending student\'s full name as {studentFN} {studentLN}. ')
+
+    # Student's ID
+    student_id = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[4]/div/div[2]/div/div/input')
+    student_id.send_keys(studentID)
+    print(f'Sending student\'s id as {studentID}.')
+
+    # Student's Email
+    student_email_input = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[5]/div/div[2]/div/div/input')
+    student_email_input.send_keys(studentEmail)
+    print(f'Sending student\'s email as {studentEmail}.')
+
+    # Student's Phone
+    student_phone_input = driver.find_element_by_xpath(
+        '//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[6]/div/div[2]/div/div/input')
+    student_phone_input.send_keys(studentPhone)
+    print(f'Sending student\'s phone as {studentPhone}.')
+
+    # Requester Name
+    requester_input = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[7]/div/div[2]/div/div/input')
+    requester_input.send_keys(requester)
+    print(f'Form prepared by {requester}')
+
+    if status_text == "Undeclared":
+        print('from Undeclared')
+        from_college_code = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[8]/div/div[2]/div/div[2]/div/label/input')
+        from_college_code.click()
+        from_degree_code = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[9]/div/div[2]/div/div/input')
+        from_degree_code.send_keys('00')
+        teacher_cert = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[21]/div/div[2]/div/div[2]/div/label/input')
+        teacher_cert.click()
+    else:
+        pass
+
+    print(f'Declaring student with College Code of {collegeCode}')
+    if collegeCode == "AS":
+        print('This is the AS option')
+        college_to_btn = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[22]/div/div[2]/div/div[1]/div/label/input')
+        college_to_btn.click()
+    else:
+        pass
+
+    to_degree_code = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[23]/div/div[2]/div/div/input')
+    to_degree_code.send_keys(degreeCode)
+    print(f'Sending degree code of {degreeCode}')
+    print(f'Sending majorCode of {majorCode}')
+
+    driver.close()
     return
 
 
@@ -238,7 +238,7 @@ def selenium():
     majorCode = request.form['majorCode']
     # majorConcentration = request.form['majorConcentration']
     requester = current_user.name
-    result = q.enqueue(to_background)
+    result = q.enqueue(to_background, e_ID, e_password, effective_term_text, studentFN, studentLN, studentID, studentEmail, studentPhone, requester, status_text, collegeCode, degreeCode, majorCode)
     return render_template('declare.html', cctitle="Declaration")
 
 
