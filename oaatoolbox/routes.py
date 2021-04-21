d6514861772d1f07c410a527c0cebd1f96d51807
cpecue <caleb.pecue.1@gmail.com>
2021-04-21 13:08:09 -0500

    Added a lot to fix the form.

--- oaatoolbox/routes.py
+++ oaatoolbox/routes.py
@@ -116,10 +116,12 @@ def to_background(to_teacher_cert, primarySecondary, e_ID, e_password, effective
     time.sleep(4)
 
     if status_text == "Undeclared":
-        from_college_code = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div[2]/div[1]/div/div[2]/div/div[2]/div/label/input')
-        print('from Undeclared')
-        from_college_code.click()
-        print('Clicked AS-Undeclared')
+        # from_college_code = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div[2]/div[1]/div/div[2]/div/div[2]/div/label/input')
+        # print('from Undeclared')
+        # from_college_code.click()
+        # print('Clicked AS-Undeclared')
+        from_degree = driver.find_element_by_xpath('/html/body/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[2]/div[2]/div/div[2]/div/div/input')
+        from_degree.send_keys('testing')
         print('set all values for undeclared student.')
     else:
         pass
