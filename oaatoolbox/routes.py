84dd01aa73f5b278f86721bb4ef701bb7122eab3
cpecue <caleb.pecue.1@gmail.com>
2021-04-29 13:35:36 -0500

    Added a lot to fix the form.

--- oaatoolbox/routes.py
+++ oaatoolbox/routes.py
@@ -191,8 +191,8 @@ def to_background(to_teacher_cert, primarySecondary, e_ID, e_password, effective
     senior_with_degree = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[2]/div/div[40]/div/div[2]/div/div[2]/div/label/input')
     senior_with_degree.click()
 
+    time.sleep(5)
     submit_btn = driver.find_element_by_xpath('//*[@id="form-container"]/div/div/div/div/div[1]/div[2]/div[3]/div[1]/button/div')
-    submit_btn.send_keys(Keys.ENTER)
     submit_btn.click()
     print('Sending data!')
     time.sleep(10)
